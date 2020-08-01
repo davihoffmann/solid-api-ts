274e66d274acefca4293b559c97dea2fa9657e88
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-31 21:51:06 -0300

    finalizado exemplo de implementação do solid pattern

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port : 2525,
+      auth: {
+        user: 'f7ffb2b8712e98',
+        pass: 'cbd145c5d61f1d'
+      }
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body
+    })
+  }
+
+}
